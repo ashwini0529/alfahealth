6222d5f83f5344032f1127271a482265ca75fff2
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-06-08 19:56:29 +0530

    Updated crawlling methods

--- alfahealth/spiders/health.py
+++ alfahealth/spiders/health.py
@@ -5,9 +5,15 @@ import scrapy
 class HealthSpider(scrapy.Spider):
     name = "health"
     allowed_domains = ["www.bodybuilding.com"]
-    start_urls = (
-        'http://www.bodybuilding.com/',
-    )
+    start_urls = [
+        'http://www.bodybuilding.com/exercises/detail/view/name/ab-roller',
+    ]
 
     def parse(self, response):
-        pass
+       for sel in response.xpath('//ul/li'):
+       	item = alfaItem()
+       	item['title']=sel.xpath('a/text()').extract()
+       	item['link'] = sel.xpath('a/@href').extract()
+       	item['desc'] = sel.xpath('text()').extract()
+        yield item
+
